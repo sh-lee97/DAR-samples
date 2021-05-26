58f5e012ec296991e6743b2d67ba0a45ba2e287e
shlee <shlee@MacBook-Pro-2.local>
2021-05-26 23:47:02 +0900

    wow

---
+++ true_lti.py
@@ -0,0 +1,15 @@
+import numpy as np
+import soundfile as sf
+import glob
+from scipy import signal
+
+dirs = glob.glob('audio/fdn/*/*')
+
+for dir_folder in dirs:
+    lti_ir_dir = glob.glob(dir_folder + '/*true_lti_estimation.wav')[0]
+    deco_str = lti_ir_dir[-28:-23]
+    print(deco_str)
+    lti_ir, sr = sf.read(lti_ir_dir)
+    dspeech, _ = sf.read(glob.glob(dir_folder + '/*dry_speech.wav')[0])
+    lti_cspeech = signal.convolve(dspeech, lti_ir)
+    sf.write(dir_folder + '/' + deco_str + 'true_lti_convolved_speech.wav', lti_cspeech, sr)
